e1c732c6e3b9557f3da34935d779ba93c54a9110
Alex Groat <alex.groat.95@gmail.com>
2022-01-10 17:34:22 +1030

    Update

--- snake.js
+++ snake.js
@@ -1,5 +1,43 @@
 
+class Snake {
+    constructor(x, y, size) {
+        this.x = x
+        this.y = y
+        this.size = size
+        this.tail = [{x: this, y: this.y}]
+        this.rotateX = 0
+        this.rotateY = 1
 
+    }
+
+    move () {
+        var newRect;
+        if(this.rotateX == 1) {
+            newRect = {
+                x: this.tail[this,tail.length - 1].x + this.size,
+                y: this.tail[this,tail.length - 1].y
+            }
+
+        } else if(this.rotateX == -1) {
+            newRect = {
+                x: this.tail[this,tail.length - 1].x - this.size,
+                y: this.tail[this,tail.length - 1].y
+            }
+
+        } else if(this.rotateY == 1) {
+            newRect = {
+                x: this.tail[this,tail.length - 1].x,
+                y: this.tail[this,tail.length - 1].y + this.size
+            }
+
+        } else if(this.rotateX == -1) {
+            newRect = {
+                x: this.tail[this,tail.length - 1].x,
+                y: this.tail[this,tail.length - 1].y - this.size
+            }
+        } 
+    }
+}
 
 
 var canvas = document.getElementById("canvas")
@@ -16,4 +54,9 @@ window.onload = () => {
 
 function gameLoop () {
     setInterval(show, 1000/15) // 15 is our fps value+
+}
+
+function show () {
+    update();
+    draw();
 }
